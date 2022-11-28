145846ed5362ad597eeea721839a32667cfb2a39
Bonika Desai <7951bonikadesai@gmail.com>
2022-11-28 19:28:54 +0530

    Managing Input/Output

---
+++ Unit-3/UL3_2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  int a,b;
+  clrscr();
+
+  printf("Enter the value of A :");
+  scanf("%d",&a);
+  printf("Enter the value of B :");
+  scanf("%d",&b);
+
+  a=a+b;
+  b=a-b;
+  a=a-b;
+
+  printf("A : %d\nB : %d",a,b);
+  getch();
+}
